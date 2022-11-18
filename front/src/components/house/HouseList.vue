27691fc4af1cef9f5eb4d3948c533bd13008c1f6
Mccreate <minwoo6951@naver.com>
2022-11-18 10:31:51 +0900

    css

--- front/src/components/house/HouseList.vue
+++ front/src/components/house/HouseList.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>AptList입니다.</div>
+  <div class="main-div">
+    <b-navbar> </b-navbar>
+    <section></section>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+.main-div {
+  min-height: 64vh;
+}
+</style>
